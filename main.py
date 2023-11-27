1b9099bac2f1e546e30b59bf132863e40ff498a7
sphrl <alenabragina2000@gmail.com>
2023-11-27 20:35:10 +0300

    all

--- main.py
+++ main.py
@@ -1,59 +1,47 @@
-import tkinter as tk
-from tkinter import messagebox
-from sqrEq import sqrEquation
+
+from tkinter import *
+import string
+import random
+
+def clicked():
+    btn.destroy()
+    lbl.destroy()
+    ei = str(txt.get())
+    letter1 = random.choice(string.ascii_uppercase) 
+    letter2 = random.choice(string.ascii_uppercase)
+    letter3 = random.choice(string.ascii_uppercase)
+    letter4 = random.choice(string.ascii_uppercase)
+    first_module = ei[3] + ei[4] + ei[5] + letter1 + letter2
+    second_module = ei[0] + ei[1] + ei[2] + letter3 + letter4
+    third_module = str(int(ei[3] + ei[4] + ei[5]) + int(ei[0] + ei[1] + ei[2]))
+    if len(third_module) < 4:
+        third_module = '0' + third_module
+    key = first_module + '-' + second_module + ' ' + third_module
+    lbl_1 = Label(window, text=key, font=("Times New Roman", 18))
+    lbl_1.grid(column=1, row=0)
+    txt.destroy()
 
 
-def close():
-    window.destroy()
+window = Tk()
+window.title('Subway Surf KEY')
 
+window.geometry("1200x675")
+bg = PhotoImage(file = "backsubway.png")
+label1 = Label(window, image=bg)
+label1.place(x = 0, y = 0)
+ 
+# window.geometry("1200x675")
+# bg = PhotoImage(file = "Tf16.gif")
+# label1 = Label(window, image=bg)
+# label1.place(x = 0, y = 0)
 
-def calc():
-    A = float(arg_A.get())
-    B = float(arg_B.get())
-    C = float(arg_C.get())
-    if A == 0.0:
-        tk.messagebox.showwarning('Error', 'Division by zero!')
-    else:
-        lbl_result.configure(text=sqrEquation(A, B, C))
+lbl = Label(window, text="Please enter a DEC-number of 6 characters: ", font=("Times New Roman", 18))
+lbl.grid(column=0, row=0)
 
+txt = Entry(window,width = 10)
+txt.grid(column=2, row=0)
 
-window = tk.Tk()
-window.geometry('576x360')
-bg_img = tk.PhotoImage(file='bg_pic.png')
-
-lbl_bg = tk.Label(window, image=bg_img)
-lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
-
-frame = tk.Frame(window)
-frame.place(relx=0.5, rely=0.5, anchor='center')
-
-lbl_A = tk.Label(frame, text='A', font=('Arial', 30), bg='blue', fg='white')
-lbl_A.grid(column=0, row=0, padx=10, pady=15)
-arg_A = tk.Entry(frame, width=10)
-arg_A.insert(0, '1')
-arg_A.grid(column=0, row=1, padx=10, pady=15)
-
-lbl_B = tk.Label(frame, text='B', font=('Arial', 30))
-lbl_B.grid(column=1, row=0, padx=10, pady=15)
-arg_B = tk.Entry(frame, width=10)
-arg_B.insert(0, '0')
-arg_B.grid(column=1, row=1, padx=10, pady=15)
-
-lbl_C = tk.Label(frame, text='C', font=('Arial', 30))
-lbl_C.grid(column=2, row=0, padx=10, pady=15)
-arg_C = tk.Entry(frame, width=10)
-arg_C.insert(0, '0')
-arg_C.grid(column=2, row=1, padx=10, pady=15)
-
-lbl_roots = tk.Label(frame, text='Result:')
-lbl_roots.grid(column=1, row=2)
-lbl_result = tk.Label(frame, text='None yet.', font=('Arial', 10))
-lbl_result.grid(column=2, row=2)
-
-btn_calc = tk.Button(frame, text='Calculate', command=calc)
-btn_calc.grid(column=0, row=3, padx=10, pady=15)
-btn_exit = tk.Button(frame, text='Cancel', command=close)
-btn_exit.grid(column=2, row=3, padx=10, pady=15)
-
+btn = Button(window, text="enter", font=("Times New Roman", 18),command=clicked)
+btn.grid(column=3, row=0)
 
 window.mainloop()
